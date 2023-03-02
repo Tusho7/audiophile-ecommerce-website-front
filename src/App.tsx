4ad8558c3b367f2818568f3517c4c37eb0427df3
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2023-03-02 12:59:51 +0400

    feat: Get data from backend

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,27 @@ import SignUp from "./Components/SingUp";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Category from "./Components/Category";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get(
+          "https://audiophile-ecommerce-tunt.onrender.com/api/products"
+        );
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, []);
+
   return (
     <div className="App">
       <Routes>
