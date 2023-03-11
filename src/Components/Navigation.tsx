ce45d33a469725a3ead6535a998d8835e5f3279b
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2023-03-11 23:20:10 +0400

    feat: Create hamburger in Navigation.tsx

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,26 +1,45 @@
+import { useState } from "react";
+import Category from "./Category";
 import { NavigationContainer } from "./styles";
 function Navigation() {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleVisible = () => {
+    setToggle(!toggle);
+    console.log(toggle);
+    <div>
+      <h2>Hello</h2>
+    </div>;
+  };
+
   return (
-    <NavigationContainer>
-      <div>
-        <img
-          src={`https://audiophile-ecommerce-tunt.onrender.com/allImages/Icons/icon-hamburger.svg`}
-        />
-      </div>
+    <div>
+      <NavigationContainer>
+        <div onClick={toggleVisible}>
+          <img
+            src={`https://audiophile-ecommerce-tunt.onrender.com/allImages/Icons/icon-hamburger.svg`}
+          />
+        </div>
 
-      <div>
-        <img
-          src={`https://audiophile-ecommerce-tunt.onrender.com/allImages/Icons/logo.svg`}
-        />
-      </div>
+        <div>
+          <img
+            src={`https://audiophile-ecommerce-tunt.onrender.com/allImages/Icons/logo.svg`}
+          />
+        </div>
 
-      <div>
-        <img
-          src={`https://audiophile-ecommerce-tunt.onrender.com/allImages/Icons/icon-cart.svg`}
-        />
-      </div>
-    </NavigationContainer>
+        <div>
+          <img
+            src={`https://audiophile-ecommerce-tunt.onrender.com/allImages/Icons/icon-cart.svg`}
+          />
+        </div>
+      </NavigationContainer>
+
+      {toggle && <Category />}
+    </div>
   );
 }
 
 export default Navigation;
+function usestate(arg0: boolean): [any, any] {
+  throw new Error("Function not implemented.");
+}
